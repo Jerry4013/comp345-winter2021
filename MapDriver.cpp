66c24a1ac09fabaf44719a60b6f6c45e4eb8ba3a
JovieL25 <lllllinbb@gmail.com>
2021-02-17 23:27:49 -0500

    fix map

--- MapDriver.cpp
+++ MapDriver.cpp
@@ -12,32 +12,36 @@ int mapDriver::main() {
     HelperFunctionMap helper;
     testmap.CreateCountryMatrix();
     testmap.CreateContinentMatrix();
-    Continent continent1 = Continent("Continent0" , 0);
-    Continent continent2 =Continent("Continent1" , 1);
-    Continent continent3 =Continent("Continent2" , 2);
-    Continent continent4 =Continent("Continent3" , 3);
-    Continent continent5 =Continent("Continent4" , 4);
+    Continent continent1 = Continent("Continent1" , 1);
+    Continent continent2 =Continent("Continent2" , 2);
+    Continent continent3 =Continent("Continent3" , 3);
+    Continent continent4 =Continent("Continent4" , 4);
+    Continent continent5 =Continent("Continent5" , 5);
+    Continent continent6 =Continent("Continent6" , 6);
 
     testmap.AddContinent(&continent1);
     testmap.AddContinent(&continent2);
     testmap.AddContinent(&continent3);
     testmap.AddContinent(&continent4);
     testmap.AddContinent(&continent5);
+    testmap.AddContinent(&continent6);
 
-
+    /*
     helper.AddEdgesContinents(1,2);
     helper.AddEdgesContinents(2,3);
     helper.AddEdgesContinents(3,4);
     helper.AddEdgesContinents(4,1);
     helper.AddEdgesContinents(1,3);
-    helper.PrintContinentMatrix();
+     */
 
-    Territory *T0 = new Territory("Territory1",0,0);
-    Territory *T1 = new Territory("Territory2",1,0);
-    Territory *T2 = new Territory("Territory3",2,1);
-    Territory *T3 = new Territory("Territory4",3,2);
-    Territory *T4 = new Territory("Territory5",4,3);
-    Territory *T5 = new Territory("Territory6",5,4);
+
+    Territory *T0 = new Territory("Territory1",1,1);
+    Territory *T1 = new Territory("Territory2",2,1);
+    Territory *T2 = new Territory("Territory3",3,2);
+    Territory *T3 = new Territory("Territory4",4,3);
+    Territory *T4 = new Territory("Territory5",5,4);
+    Territory *T5 = new Territory("Territory6",6,5);
+    Territory *T6 = new Territory("Territory7",7,6);
 
     T0->SetNumTroops("player1",1);
     T1->SetNumTroops("player2",2);
@@ -50,6 +54,7 @@ int mapDriver::main() {
     T3->GetNumTroops("player4");
     T4->GetNumTroops("player1");
     T5->GetNumTroops("player2");
+    T6->GetNumTroops("player3");
 
     continent1.AddTerritory(T0);
     continent1.AddTerritory(T1);
@@ -57,17 +62,31 @@ int mapDriver::main() {
     continent3.AddTerritory(T3);
     continent4.AddTerritory(T4);
     continent5.AddTerritory(T5);
+    continent6.AddTerritory(T6);
 
     helper.AddEdgesCountry(T0,T1);
+    helper.AddEdgesCountry(T1,T0);
+
     helper.AddEdgesCountry(T1,T2);
+    helper.AddEdgesCountry(T2,T1);
+
     helper.AddEdgesCountry(T2,T3);
+    helper.AddEdgesCountry(T3,T2);
+
     helper.AddEdgesCountry(T3,T4);
+    helper.AddEdgesCountry(T4,T3);
+
     helper.AddEdgesCountry(T4,T5);
+    helper.AddEdgesCountry(T5,T4);
+
+    helper.AddEdgesCountry(T5,T6);
+    helper.AddEdgesCountry(T6,T5);
+
     helper.PrintContinentMatrix();
     helper.PrintTerritoryMatrix();
 
     testmap.Validate(&testmap);
 
-
+    return 0;
 
 }
