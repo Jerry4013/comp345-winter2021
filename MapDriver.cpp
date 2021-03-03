f8ef2ccbc4ea509f78501f0c862ad032135e3fee
Jerry4013 <jingchao.zhang@mail.concordia.ca>
2021-03-03 13:17:44 -0500

    test Map

--- MapDriver.cpp
+++ MapDriver.cpp
@@ -8,136 +8,47 @@ using namespace std;
 #include "MapDriver.h"
 
 int mapDriver::main() {
-    Map testMap = Map("test", 6, 7);
-    Continent continent1 = Continent("Continent1" , 1);
-    Continent continent2 = Continent("Continent2" , 2);
-    Continent continent3 = Continent("Continent3" , 3);
-    Continent continent4 = Continent("Continent4" , 4);
-    Continent continent5 = Continent("Continent5" , 5);
-    Continent continent6 = Continent("Continent6" , 6);
+    vector<string> mapNames = {"volcano", "redForest", "village", "mountain"};
 
-    testMap.AddContinent(&continent1);
-    testMap.AddContinent(&continent2);
-    testMap.AddContinent(&continent3);
-    testMap.AddContinent(&continent4);
-    testMap.AddContinent(&continent5);
-    testMap.AddContinent(&continent6);
+    vector<string> continentNames = {"volcano1", "volcano2", "redForest", "village", "mountain1", "mountain2"};
+    vector<int> belongsToMapId = {1, 1, 2, 3, 4, 4};
 
-    /*
-    helper.AddEdgesContinents(1,2);
-    helper.AddEdgesContinents(2,3);
-    helper.AddEdgesContinents(3,4);
-    helper.AddEdgesContinents(4,1);
-    helper.AddEdgesContinents(1,3);
-     */
+    vector<string> territoryNames = {"volcano1_upper1", "volcano1_upper2", "volcano2_upper", "volcano2_middle1",
+                                     "volcano2_middle2", "volcano2_bottom1", "volcano2_bottom2", "redforest_upper",
+                                     "redforest_middle", "redforest_bottom", "village_upper1", "village_upper2",
+                                     "village_middle", "village_bottom1", "village_bottom2", "mountain1_upper",
+                                     "mountain1_middle", "mountain2"};
+    vector<int> belongsToContinentId = {1, 1, 2, 2, 2, 2, 2, 3, 3, 3, 4, 4, 4, 4, 4, 5, 5, 6};
+    vector<vector<int>> edges = {{1, 2}, {2, 5}, {3, 4}, {3, 5}, {4, 5}, {5, 6}, {6, 7}, {4, 9}, {8, 9}, {9, 10},
+                                 {7, 11}, {11, 12}, {11, 13}, {12, 13}, {12, 14}, {13, 14}, {14, 15}, {13, 16},
+                                 {16, 17}, {17, 18}, {10, 16}};
 
+//    vector<Map*> maps(mapNames.size());
+//    for (int i = 0; i < maps.size(); i++) {
+//        maps[i] = new Map(i + 1, mapNames[i]);
+//    }
+    string mapName = "testMap";
+    Map* testMap1 = new Map(1, mapName);
+    vector<Continent*> continents(continentNames.size());
+    for (int i = 0; i < continents.size(); i++) {
+        continents[i] = new Continent(i + 1, continentNames[i], belongsToMapId[i]);
+        testMap1->addContinent(continents[i]);
+    }
 
-    Territory *T0 = new Territory("Territory1",1,1);
-    Territory *T1 = new Territory("Territory2",2,1);
-    Territory *T2 = new Territory("Territory3",3,2);
-    Territory *T3 = new Territory("Territory4",4,3);
-    Territory *T4 = new Territory("Territory5",5,4);
-    Territory *T5 = new Territory("Territory6",6,5);
-    Territory *T6 = new Territory("Territory7",7,6);
+    vector<Territory*> territories(territoryNames.size());
+    for (int i = 0; i < territories.size(); i++) {
+        territories[i] = new Territory(i + 1, territoryNames[i], belongsToContinentId[i], 2);
+        continents[belongsToContinentId[i] - 1]->addTerritory(territories[i]);
+        testMap1->addTerritory(territories[i]);
+    }
 
-    T0->SetNumTroops("player1",1);
-    T1->SetNumTroops("player2",2);
-    T2->SetNumTroops("player3",3);
-    T3->SetNumTroops("player4",4);
-
-    T0->getArmiesOfPlayer("player1");
-    T1->getArmiesOfPlayer("player2");
-    T2->getArmiesOfPlayer("player3");
-    T3->getArmiesOfPlayer("player4");
-    T4->getArmiesOfPlayer("player1");
-    T5->getArmiesOfPlayer("player2");
-    T6->getArmiesOfPlayer("player3");
-
-    continent1.AddTerritory(T0);
-    continent1.AddTerritory(T1);
-    continent2.AddTerritory(T2);
-    continent3.AddTerritory(T3);
-    continent4.AddTerritory(T4);
-    continent5.AddTerritory(T5);
-    continent6.AddTerritory(T6);
-
-    testMap.addTerritoryEdges(T0, T1);
-    testMap.addTerritoryEdges(T1, T0);
-    testMap.addTerritoryEdges(T1, T2);
-    testMap.addTerritoryEdges(T2, T1);
-    testMap.addTerritoryEdges(T2, T3);
-    testMap.addTerritoryEdges(T3, T2);
-    testMap.addTerritoryEdges(T3, T4);
-    testMap.addTerritoryEdges(T4, T3);
-    testMap.addTerritoryEdges(T4, T5);
-    testMap.addTerritoryEdges(T5, T4);
-    testMap.addTerritoryEdges(T5, T6);
-    testMap.addTerritoryEdges(T6, T5);
-
-    testMap.PrintContinentMatrix();
-    testMap.PrintTerritoryMatrix();
-
-//    testMap.validate(&testMap);
-    testMap.dfsValidate();
+    for (auto & edge : edges) {
+        testMap1->addTerritoryEdges(edge[0], edge[1]);
+    }
+    testMap1->printTerritoryAdjacencyList();
+    testMap1->printContinentAdjacencyList();
     
-    Map testMap2 = Map("test", 6, 7);
-    Continent continent1_2 = Continent("Continent1" , 1);
-    Continent continent2_2= Continent("Continent2" , 2);
-    Continent continent3_2 = Continent("Continent3" , 3);
-    Continent continent4_2 = Continent("Continent4" , 4);
-    Continent continent5_2 = Continent("Continent5" , 5);
-    Continent continent6_2 = Continent("Continent6" , 6);
-    
-    testMap2.AddContinent(&continent1_2);
-    testMap2.AddContinent(&continent2_2);
-    testMap2.AddContinent(&continent3_2);
-    testMap2.AddContinent(&continent4_2);
-    testMap2.AddContinent(&continent5_2);
-    testMap2.AddContinent(&continent6_2);
-    
-    Territory *T0_2 = new Territory("Territory1",1,1);
-    Territory *T1_2 = new Territory("Territory2",2,1);
-    Territory *T2_2 = new Territory("Territory3",3,2);
-    Territory *T3_2 = new Territory("Territory4",4,3);
-    Territory *T4_2 = new Territory("Territory5",5,4);
-    Territory *T5_2 = new Territory("Territory6",6,5);
-    Territory *T6_2 = new Territory("Territory7",7,6);
-    
-    T0_2->SetNumTroops("player1",1);
-    T1_2->SetNumTroops("player2",2);
-    T2_2->SetNumTroops("player3",3);
-    T3_2->SetNumTroops("player4",4);
-
-    T0_2->getArmiesOfPlayer("player1");
-    T1_2->getArmiesOfPlayer("player2");
-    T2_2->getArmiesOfPlayer("player3");
-    T3_2->getArmiesOfPlayer("player4");
-    T4_2->getArmiesOfPlayer("player1");
-    T5_2->getArmiesOfPlayer("player2");
-    T6_2->getArmiesOfPlayer("player3");
-    
-    continent1_2.AddTerritory(T0_2);
-    continent1_2.AddTerritory(T1_2);
-    continent2_2.AddTerritory(T2_2);
-    continent3_2.AddTerritory(T3_2);
-    continent4_2.AddTerritory(T4_2);
-    continent5_2.AddTerritory(T5_2);
-    continent6_2.AddTerritory(T6_2);
-
-    testMap2.addTerritoryEdges(T0_2, T1_2);
-    testMap2.addTerritoryEdges(T1_2, T0_2);
-    testMap2.addTerritoryEdges(T1_2, T2_2);
-    testMap2.addTerritoryEdges(T2_2, T1_2);
-    testMap2.addTerritoryEdges(T4_2, T5_2);
-    testMap2.addTerritoryEdges(T5_2, T4_2);
-    testMap2.addTerritoryEdges(T5_2, T6_2);
-    testMap2.addTerritoryEdges(T6_2, T5_2);
-    
-    testMap2.PrintContinentMatrix();
-    testMap2.PrintTerritoryMatrix();
-    
-    //    testMap.validate(&testMap);
-    testMap2.dfsValidate();
+    // TODO: need more tests
     return 0;
 
 }
